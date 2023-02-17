cacf1a3173c8a2c347ed12c4662603bace4c24c1
Leon <ln4227@uncw.edu>
2023-02-16 21:39:01 -0500

    Update p1_sol.py

--- p1_sol.py
+++ p1_sol.py
@@ -1,37 +1,25 @@
 import numpy as np
 import rbm
+
 # define rotation values
 roll = np.pi / 2
 pitch = np.pi / 2
 yaw = np.pi / 2
 
+# Output format
+np.set_printoptions(precision = 3, suppress = True)
+
 # rotation matrix about x-axix: roll
-R_x = main.rot_x(roll)
+R_x = rbm.rot_x(roll)
 # rotation matrix about y-axis: pitch
-R_y = main.rot_y(pitch)
+R_y = rbm.rot_y(pitch)
 # rotation matrix about z-axis: yaw
-R_z = main.rot_z(yaw)
+R_z = rbm.rot_z(yaw)
 # final rotation matrix
-print("Rx:\n",R_x)
-print("R_y:\n", R_y)
-print("Rz:\n", R_z)
+rot = np.matmul(R_z, R_y, R_x)
 
-# # rotation matrix about x-axix: roll
-# R_x = np.array([[1, 0, 0],
-#                 [0, np.cos(roll), -np.sin(roll)],
-#                 [0, np.sin(roll), np.cos(roll)]])
+print("Roll rotation matrix:\n", R_x)
+print("Pitch rotation matrix:\n", R_y)
+print("Yaw rotation matrix:\n", R_z)
+print("Final rotation matrix:\n", rot)
 
-# # rotation matrix about y-axis: pitch
-# R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-#                 [0, 1, 0],
-#                 [-np.sin(pitch), 0, np.cos(pitch)]])
-
-# # rotation matrix about z-axis: yaw
-# R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-#                 [np.sin(yaw), np.cos(yaw), 0],
-#                 [0, 0, 1]])
-
-# # final rotation matrix
-# rot = np.matmul(np.matmul(R_z, R_y), R_x)
-
-# print("Rotation:\n", rot)
