2c5f315795056f2519272521fd3c1e8e83732c2f
Leon <ln4227@uncw.edu>
2023-02-16 22:31:56 -0500

    Update p2_sol.py

--- p2_sol.py
+++ p2_sol.py
@@ -1,47 +1,94 @@
 import numpy as np
 
-def translate(tx, ty):
+def translate_x(tx):
     """
-    Returns a homogeneous translation matrix for a 2D translation along x and y axes
-    
+    Returns a homogeneous translation matrix for a 3D translation along x axes
+   
     Parameters:
     - tx: Translation along x axis
-    - ty: Translation along y axis
-    
+   
     Returns:
-    - A 3x3 homogeneous translation matrix
+    - A 4x4 homogeneous translation matrix
     """
-    return np.array([[1, 0, tx],
-                     [0, 1, ty],
-                     [0, 0, 1]])
+    return np.array([[1, 0, 0, tx],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+                     
+def translate_y(ty):
+    """
+    Returns a homogeneous translation matrix for a 3D translation along y axes
+   
+    Parameters:
+    - ty: Translation along y axis
+   
+    Returns:
+    - A 4x4 homogeneous translation matrix
+    """
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, ty],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
 
-def rotate(theta):
+def translate_z(tz):
     """
-    Returns a homogeneous rotation matrix for a 2D rotation about the origin
-    
+    Returns a homogeneous translation matrix for a 3D translation along z axes
+   
+    Parameters:
+    - tz: Translation along z axis
+   
+    Returns:
+    - A 4x4 homogeneous translation matrix
+    """
+    return np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, tz],
+                     [0, 0, 0, 1]])
+
+def rotate_x(theta):
+    """
+    Returns a homogeneous rotation matrix for a 3D rotation about the x-axis
+   
     Parameters:
     - theta: Angle of rotation in radians
-    
+   
     Returns:
-    - A 3x3 homogeneous rotation matrix
+    - A 4x4 homogeneous rotation matrix
     """
     cos, sin = np.cos(theta), np.sin(theta)
-    return np.array([[cos, -sin, 0],
-                     [sin, cos, 0],
-                     [0, 0, 1]])
+    return np.array([[1, 0, 0, 0],
+                     [0, cos, -sin, 0],
+                     [0, sin, cos, 0],
+                     [0, 0, 0, 1]])
 
-def transform(tx, ty, theta):
+def rotate_y(theta):
     """
-    Returns a homogeneous transformation matrix for a 2D translation followed by a 2D rotation
-    
+    Returns a homogeneous rotation matrix for a 3D rotation about the y-axis
+   
     Parameters:
-    - tx: Translation along x axis
-    - ty: Translation along y axis
     - theta: Angle of rotation in radians
-    
+   
     Returns:
-    - A 3x3 homogeneous transformation matrix
+    - A 4x4 homogeneous rotation matrix
     """
-    return np.matmul(translate(tx, ty), rotate(theta))
+    cos, sin = np.cos(theta), np.sin(theta)
+    return np.array([[cos, 0, sin, 0],
+                     [0, 1, 0, 0],
+                     [-sin, 0, cos, 0],
+                     [0, 0, 0, 1]])
 
-
+def rotate_z(theta):
+    """
+    Returns a homogeneous rotation matrix for a 3D rotation about the z-axis
+   
+    Parameters:
+    - theta: Angle of rotation in radians
+   
+    Returns:
+    - A 4x4 homogeneous rotation matrix
+    """
+    cos, sin = np.cos(theta), np.sin(theta)
+    return np.array([[cos, -sin, 0, 0],
+                     [sin, cos, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
